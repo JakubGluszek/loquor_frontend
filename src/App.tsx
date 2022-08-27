f32e939db185bea8c3b4c93b7ff98397cd9cbc6f
JacobG03 <jacobgluszek03@gmail.com>
2022-08-27 14:33:30 +0200

    new stun server

--- src/App.tsx
+++ src/App.tsx
@@ -526,7 +526,7 @@ const HomeView: React.FC<HomeViewProps> = ({ me, socket }) => {
       pc: new RTCPeerConnection({
         iceServers: [
           {
-            urls: "stun:stun4.l.google.com:19302",
+            urls: "stun:stun.l.google.com:19302",
           },
         ],
       }),
